2f950d110276586099c25e87460efeb8c684e6d9
Tony <tony_a_wilkes@outlook.com>
2024-03-11 13:17:01 +0100

    Updated Examples

--- R/class_mutable_atomic.R
+++ R/class_mutable_atomic.R
@@ -39,20 +39,7 @@
 #' `is.mutable_atomic` returns `FALSE` otherwise.
 #'
 #'
-#' @examples
-#' 
-#' x <- mutable_atomic(1:20, dim = c(5,4), dimnames = list(letters[1:5], letters[1:4]))
-#' x
-#' 
-#' x <- matrix(1:10, ncol = 2)
-#' x <- as.mutable_atomic(x)
-#' is.mutable_atomic(x)
-#' print(x)
-#' x[,1]
-#' x[] <- as.double(x) # notifies the user a copy is being made
-#' print(x) # "typeof" attribute adjusted accordingly, and class still present
-#' 
-#' 
+#' @example inst/examples/class_mutable_atomic.R
 #' 
 
 
