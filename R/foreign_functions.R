50fc36af16cfe84c24b671a0e1ad3007a1ea6763
Tony <tony_a_wilkes@outlook.com>
2024-03-29 17:49:33 +0100

    Added `sb2_before.array()` and `sb2_after.array()`, and added tests for these also. Added tests for data.frame-like coercion types. Tweaked the documentation here and there a bit.

--- R/foreign_functions.R
+++ R/foreign_functions.R
@@ -1,37 +1,85 @@
 
+
 #' @keywords internal
 #' @noRd
-.abind.recursive <- function(arg.list, along=N) {
-  # adapted from the 'abind' package
-  # rev.along <- NULL
-  # new.names <- NULL
+.abind.recursive <- function(
+    arg.list, along = N, rev.along = NULL, new.names = NULL,
+    use.first.dimnames = FALSE, hier.names = FALSE, use.dnns = FALSE)
+{
+  # This function is a modified version of abind::abind.
+  # The modification is different in 4 ways:
+  # 1) .abind.recursive can bind recursive arrays (i.e. dimensional lists), whereas abind::abind can't.
+  # 2) .abind.recursibe is faster, due to more efficient code, and through 'collapse' package.
+  # 3) The main input is a list, instead of an ellipsis.
+  # 4) some arguments have been disabled or set at a fixed value for simplicity.
+  
+  #make.names <- FALSE
+  # use.anon.names <- FALSE
   # force.array <- TRUE
-  # make.names <- use.anon.names <- FALSE
-  use.first.dimnames <- FALSE
-  #  hier.names = FALSE
-  use.dnns <- FALSE
-  hier.names <- 'no'
+  
+  if (is.character(hier.names)) {
+    hier.names <- match.arg(hier.names, c('before', 'after', 'none'))
+  }
+  else {
+    hier.names <- if (hier.names) 'before' else 'no'
+  }
+  
+  arg.list <- arg.list
   # have.list.arg <- TRUE
+  
   N <- max(
     1,
     vapply(arg.list, function(x) length(dim(x)), integer(1))
   )
   
+  ## N will eventually be length(dim(return.value))
+  if (!is.null(rev.along)) {
+    along <- N + 1L - rev.along
+  }
+  
+  if (along < 1L || along > N || (along > floor(along) && along < ceiling(along))) {
+    N <- N + 1L
+    along <- max(1L, min(N + 1L, ceiling(along)))
+  }
+  
+  ## this next check should be redundant, but keep it here for safety...
+  if (length(along) > 1L || along < 1L || along > (N + 1L))
+    stop(paste("\"along\" must specify one dimension of the array,",
+               "or interpolate between two dimensions of the array",
+               sep = "\n"))
+  
+  
   if (along > N || along < 1L)
     stop("along must be between 1 and ", N)
   
   pre <- seq_len(along - 1L)
-  post <- seq(to=N-1, len=N-along)
+  post <- seq(to = N - 1L, len = N - along)
   ## "perm" specifies permutation to put join dimension (along) last
   perm <- c(seq_len(N)[-along], along)
   
-  arg.names <- collapse::alloc("", length(arg.list))
+  arg.names <- names(arg.list)
+  if (is.null(arg.names)) {
+    arg.names <- collapse::alloc("", length(arg.list))
+  }
+  ## if new.names is a character vector, treat it as argument names
+  if (is.character(new.names)) {
+    arg.names[seq_along(new.names)[nchar(new.names)>0]] <-
+      new.names[nchar(new.names)>0]
+    new.names <- NULL
+  }
   
   ## Create deparsed versions of actual arguments in arg.alt.names
   ## These are used for error messages
-  arg.alt.names <- arg.names
-  arg.alt.names[arg.names==""] <- paste("X", seq_along(arg.names), sep="")[arg.names==""]
-  use.along.names <- FALSE
+  if (collapse::anyv(arg.names, "")) {
+    arg.alt.names <- arg.names
+    temp.indx <- collapse::whichv(arg.names, "")
+    arg.alt.names[temp.indx] <- paste("X", seq_along(arg.names), sep="")[temp.indx]
+  }
+  else {
+    arg.alt.names <- arg.names
+  }
+  
+  use.along.names <- !collapse::allv(arg.names, "")
   
   ## need to have here: arg.names, arg.alt.names, don't need dot.args
   
@@ -39,11 +87,11 @@
   ## arg.dimnames is a matrix of dimension names, each element of the
   ## the matrix is a character vector, e.g., arg.dimnames[j,i] is
   ## the vector of names for dimension j of arg i
-  arg.dimnames <- matrix(vector("list", N*length(arg.names)), nrow=N, ncol=length(arg.names))
+  arg.dimnames <- matrix(vector("list", N * length(arg.names)), nrow = N, ncol = length(arg.names))
   dimnames(arg.dimnames) <- list(NULL, arg.names)
   ## arg.dnns is a matrix of names of dimensions, each element is a
   ## character vector len 1, or NULL
-  arg.dnns <- matrix(vector("list", N*length(arg.names)), nrow=N, ncol=length(arg.names))
+  arg.dnns <- matrix(vector("list", N * length(arg.names)), nrow = N, ncol = length(arg.names))
   dimnames(arg.dnns) <- list(NULL, arg.names)
   dimnames.new <- vector("list", N)
   
@@ -54,18 +102,35 @@
   ## Create arg.dim as a matrix with length(dim) rows and
   ## length(arg.list) columns: arg.dim[j,i]==dim(arg.list[[i]])[j],
   ## The dimension order of arg.dim is original
-  arg.dim <- matrix(integer(1), nrow=N, ncol=length(arg.names))
+  arg.dim <- matrix(integer(1L), nrow = N, ncol = length(arg.names))
   
   for (i in seq_along(arg.list)) {
     m <- arg.list[[i]]
     m.changed <- FALSE
+    
     new.dim <- dim(m)
     if (length(new.dim) == N) {
+      if (!is.null(dimnames(m))) {
+        arg.dimnames[,i] <- dimnames(m)
+        if (use.dnns && !is.null(names(dimnames(m)))) {
+          arg.dnns[,i] <- as.list(names(dimnames(m)))
+        }
+        
+      }
       arg.dim[,i] <- new.dim
     }
-    else if (length(new.dim) == (N-1)) {
+    else if (length(new.dim) == (N - 1L)) {
       ## add another dimension (first set dimnames to NULL to prevent errors)
-      arg.dim[,i] <- c(new.dim[pre], 1, new.dim[post])
+      if (!is.null(dimnames(m))) {
+        ## arg.dimnames[,i] <- c(dimnames(m)[pre], list(NULL), dimnames(m))[post]
+        ## is equivalent to arg.dimnames[-N,i] <- dimnames(m)
+        arg.dimnames[-along,i] <- dimnames(m)
+        if (use.dnns && !is.null(names(dimnames(m))))
+          arg.dnns[-along,i] <- as.list(names(dimnames(m)))
+        ## remove the dimnames so that we can assign a dim of an extra length
+        dimnames(m) <- NULL
+      }
+      arg.dim[,i] <- c(new.dim[pre], 1L, new.dim[post])
       if (any(perm != seq_along(perm))) {
         dim(m) <- c(new.dim[pre], 1, new.dim[post])
         m.changed <- TRUE
@@ -73,7 +138,7 @@
     }
     else {
       stop("'", arg.alt.names[i], "' does not fit: should have `length(dim())'=",
-           N, " or ", N-1)
+           N, " or ", N - 1L)
     }
     
     if (any(perm != seq_along(perm))) {
@@ -94,42 +159,44 @@
   }
   
   ## find the last (or first) names for each dimensions except the join dimension
-  if (N>1) {
+  if (N > 1L)
     for (dd in seq_len(N)[-along]) {
       if (use.first.dimnames) {
         loopseq <- seq_along(arg.names)
-      } else {
+      }
+      else {
         loopseq <- rev(seq_along(arg.names))
       }
       
       for (i in loopseq) {
-        if (length(arg.dimnames[[dd,i]]) > 0) {
+        if (length(arg.dimnames[[dd,i]]) > 0L) {
           dimnames.new[[dd]] <- arg.dimnames[[dd,i]]
           if (use.dnns && !is.null(arg.dnns[[dd,i]])) {
             names(dimnames.new)[dd] <- arg.dnns[[dd,i]]
           }
+          
           break
         }
       }
     }
-  }
-  
   
   ## find or create names for the join dimension
   for (i in seq_along(arg.names)) {
     ## only use names if arg i contributes some elements
     if (arg.dim[along,i] > 0) {
       dnm.along <- arg.dimnames[[along,i]]
-      if (length(dnm.along)==arg.dim[along,i]) {
+      if (length(dnm.along) == arg.dim[along,i]) {
         use.along.names <- TRUE
-        if (hier.names=='before' && arg.names[i]!="") {
+        if (hier.names == 'before' && arg.names[i]!="") {
           dnm.along <- paste(arg.names[i], dnm.along, sep=".")
         }
-        else if (hier.names=='after' && arg.names[i]!="")
+        else if (hier.names == 'after' && arg.names[i] != "") {
           dnm.along <- paste(dnm.along, arg.names[i], sep=".")
-      } else {
+        }
+      }
+      else {
         ## make up names for the along dimension
-        if (arg.dim[along,i]==1) {
+        if (arg.dim[along,i] == 1L) {
           dnm.along <- arg.names[i]
         }
         else if (arg.names[i]=="") {
@@ -152,23 +219,41 @@
     }
   }
   ## if no names at all were given for the along dimension, use none
-  if (!use.along.names){
+  if (!use.along.names) {
     dimnames.new[along] <- list(NULL)
   }
   
-  ## Construct the output recursive array from the pieces.
   out <- do.call(c, arg.list)
   out <- array(
-    out, dim = c( arg.dim[-along,1], sum(arg.dim[along,])),
+    out, dim = c(arg.dim[-along, 1L], sum(arg.dim[along,])),
     dimnames = dimnames.new[perm]
   )
+  
   ## permute the output array to put the join dimension back in the right place
   if (any(order(perm) != seq_along(perm))) {
     out <- aperm(out, order(perm))
   }
-  if (use.dnns && !is.null(names(dimnames(out))) && any(i <- is.na(names(dimnames(out))))) {
-    names(dimnames(out))[i] <- ''
-  }
   
+  ## if new.names is list of character vectors, use whichever are non-null
+  ## for dimension names, checking that they are the right length
+  if (!is.null(new.names) && is.list(new.names)) {
+    for (dd in seq_len(N)) {
+      if (!is.null(new.names[[dd]])) {
+        if (length(new.names[[dd]]) == dim(out)[dd]) {
+          dimnames(out)[[dd]] <- new.names[[dd]]
+        }
+        else if (length(new.names[[dd]])) {
+          warning(paste("Component ", dd,
+                        " of new.names ignored: has length ",
+                        length(new.names[[dd]]), ", should be ",
+                        dim(out)[dd], sep=""))
+        }
+      }
+      if (use.dnns && !is.null(names(new.names)) && names(new.names)[dd]!='')
+        names(dimnames(out))[dd] <- names(new.names)[dd]
+    }
+  }
+  if (use.dnns && !is.null(names(dimnames(out))) && any(i <- is.na(names(dimnames(out)))))
+    names(dimnames(out))[i] <- ''
   return(out)
 }
--- inst/tinytest/test-abind.recursive.R
+++ inst/tinytest/test-abind.recursive.R
@@ -14,14 +14,26 @@ for(margin in 1:3) {
   
   dims <- c(4,4,4)
   dims[margin] <- 1
+  dimnames1 <- list(letters[1:4], rev(letters[1:4]), letters[1:4])
+  dimnames2 <- list(letters[5:8], rev(letters[5:8]), letters[5:8])
+  dimnames2[[margin]] <- "z"
   ya <- array(1:64, c(4,4,4))
   yl <- array(as.list(1:64), c(4,4,4))
   newa <- array(1:16, dim = dims)
   newl <- array(as.list(1:16), dim = dims)
+  dimnames(ya) <- dimnames1
+  dimnames(yl) <- dimnames1
+  dimnames(newa) <- dimnames2
+  dimnames(newl) <- dimnames2
   
   out <- .abind.recursive(list(yl, newl, yl), margin)
-  expected <- array(as.list(abind::abind(ya, newa, ya, along = margin)), dim = dim(out))
-  expect_equivalent(
+  pre_expected <- abind::abind(ya, newa, ya, along = margin)
+  expected <- array(
+    as.list(pre_expected),
+    dim = dim(pre_expected),
+    dimnames = dimnames(pre_expected)
+  )
+  expect_equal(
     out, expected
   ) |> errorfun()
   
