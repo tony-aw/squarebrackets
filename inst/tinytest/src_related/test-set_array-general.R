1d46ff7dfa7ba7a88041e05485bb938807ccf37e
Tony <tony_a_wilkes@outlook.com>
2024-12-07 10:33:38 +0100

    Update test-set_array-general.R

--- inst/tinytest/src_related/test-set_array-general.R
+++ inst/tinytest/src_related/test-set_array-general.R
@@ -3,11 +3,7 @@
 
 sys.source(file.path(getwd(), "source", "functions4testing.R"), envir = environment())
 enumerate <- 0
-tempfun1 <- function(x, sub, rp) {
-  x <- data.table::copy(x)
-  squarebrackets:::.rcpp_set_array_general_atomic(x, sub, dim(x), rp)
-  return(x)
-}
+
 
 tempfun2 <- function(x, sub, dims, rp) {
   ind <- idx.array(x, sub, dims)
@@ -36,6 +32,14 @@ generate_data <- function(x.len) {
 }
 
 
+# rcpp_set_array_general ====
+
+tempfun1 <- function(x, sub, rp) {
+  x <- data.table::copy(x)
+  squarebrackets:::.rcpp_set_array_general_atomic(x, sub, dim(x), rp)
+  return(x)
+}
+
 expected <- out <- list()
 i <- 1
 
@@ -55,7 +59,7 @@ for(iSample in 1:10) {
       
       x <- data.table::copy(x)
       x2 <- x
-      sb_set.array(x, sub, dims, rp = rp.lst[[iType]])
+      squarebrackets:::.rcpp_set_array_general_atomic(x, sub, dim(x), rp.lst[[iType]])
       expect_equal(x,x2) |> errorfun() # test indexing & pass-by-reference
       
       enumerate <- enumerate + 2
@@ -64,6 +68,43 @@ for(iSample in 1:10) {
   }
 }
 expect_equal(expected, out)
+
+
+expected <- out <- list()
+i <- 1
+
+for(iSample in 1:10) {
+  for(iDim in 2:8) {
+    x.dim <- sample(1:6, size = iDim, replace = TRUE)
+    x.len <- prod(x.dim)
+    x.data <- generate_data(x.len)
+    for(iType in seq_along(x.data)) {
+      x <- as.mutable_atomic(array(x.data[[iType]], x.dim))
+      sub <- lapply(x.dim, \(x) sample(1:x, max(c(1, x)), FALSE))
+      dims <- 1:length(x.dim)
+      
+      rp <- rep(rp.lst[[iType]], prod(lengths(sub)))
+      
+      out[[i]] <- tempfun1(x, sub, rp)
+      expected[[i]] <- tempfun2(x, sub, dims, rp)
+      
+      
+      x <- data.table::copy(x)
+      x2 <- x
+      squarebrackets:::.rcpp_set_array_general_atomic(x, sub, dim(x), rp)
+      expect_equal(x,x2) |> errorfun() # test indexing & pass-by-reference
+      
+      enumerate <- enumerate + 2
+      i <- i + 1
+    }
+  }
+}
+expect_equal(expected, out)
+
+
+
+
+# rcpp_set_array_16d ====
 
 tempfun1 <- function(x, sub, rp) {
   x <- data.table::copy(x)
@@ -91,7 +132,46 @@ for(iSample in 1:10) {
       
       x <- data.table::copy(x)
       x2 <- x
-      sb_set.array(x, sub, dims, rp = rp.lst[[iType]])
+      squarebrackets:::.rcpp_set_array_16d(x, rp.lst[[iType]], sub, dim(x))
+      expect_equal(x,x2) |> errorfun() # test indexing & pass-by-reference
+      
+      enumerate <- enumerate + 2
+      i <- i + 1
+    }
+  }
+}
+expect_equal(expected, out)
+
+
+tempfun1 <- function(x, sub, rp) {
+  x <- data.table::copy(x)
+  squarebrackets:::.rcpp_set_array_16d(x, rp, sub, dim(x))
+  return(x)
+}
+
+
+expected <- out <- list()
+i <- 1
+
+for(iSample in 1:10) {
+  for(iDim in 2:8) {
+    x.dim <- sample(1:6, size = iDim, replace = TRUE)
+    x.len <- prod(x.dim)
+    x.data <- generate_data(x.len)
+    for(iType in seq_along(x.data)) {
+      x <- as.mutable_atomic(array(x.data[[iType]], x.dim))
+      sub <- lapply(x.dim, \(x) sample(1:x, max(c(1, x)), FALSE))
+      dims <- 1:length(x.dim)
+      
+      rp <- rep(rp.lst[[iType]], prod(lengths(sub)))
+      
+      out[[i]] <- tempfun1(x, sub, rp)
+      expected[[i]] <- tempfun2(x, sub, dims, rp)
+      
+      
+      x <- data.table::copy(x)
+      x2 <- x
+      squarebrackets:::.rcpp_set_array_16d(x, rp, sub, dim(x))
       expect_equal(x,x2) |> errorfun() # test indexing & pass-by-reference
       
       enumerate <- enumerate + 2
