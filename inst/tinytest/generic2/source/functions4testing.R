7f6bf3fa572e960b459b9b6f32879ad3a884ae86
Tony <tony_a_wilkes@outlook.com>
2025-12-04 10:56:55 +0100

    Cleaning up GitHub's mess

--- inst/tinytest/generic2/source/functions4testing.R
+++
@@ -1,9 +0,0 @@
-errorfun <- function(tt) {
-  if(isTRUE(tt)) print(tt)
-  if(isFALSE(tt)) stop(print(tt))
-}
-
-any_empty_indices <- function(...) {
-  lst <- list(...)
-  return(squarebrackets:::.any_empty_indices(lst))
-}
